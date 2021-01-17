e4355637346f4023c7cb0a126239d5a78e25bab2
Clavijo <dev.ronny@outlook.com>
2021-01-17 12:02:22 -0500

    Agregamos Fetch

--- ajax/ajx.js
+++ ajax/ajx.js
@@ -1,4 +1,5 @@
 const listaUsuarios = document.getElementById("lista-usuarios");
+const boton = document.getElementById("boton");
 
 function reqListener() {
     const usuarios = JSON.parse(this.responseText);
@@ -10,7 +11,28 @@ function reqListener() {
     listaUsuarios.innerHTML = usuariosRender;
 }
 
-var peticion = new XMLHttpRequest();
+let peticion = new XMLHttpRequest();
 peticion.addEventListener("load", reqListener);
 peticion.open("GET", "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios");
 peticion.send();
+
+function enviarDato() {
+    peticion.open(
+        "POST",
+        "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios",
+        true
+    );
+    peticion.setRequestHeader(
+        "Content-type",
+        "application/x-www-form-urlencoded"
+    );
+    peticion.send("nombre=lunes24");
+    setTimeout(refrescar, 3000);
+}
+
+function refrescar() {
+    peticion.open("GET", "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios");
+    peticion.send();
+}
+
+boton.onclick = enviarDato;
